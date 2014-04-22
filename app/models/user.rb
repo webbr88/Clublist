2fa49e358344eede91a1235783ff5128fb090d36
Renaldo Webb <renaldo.webb@gmail.com>
2014-04-21 23:14:46 -0400

    More facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,12 @@ def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
       if registered_user
         return registered_user
       else
-        user = User.create(name:auth.extra.raw_info.name,
-                            provider:auth.provider,
-                            uid:auth.uid,
-                            email:auth.info.email,
-                            password:Devise.friendly_token[0,20]
-                          )
+         user = User.create(name:auth.extra.raw_info.name,
+                          provider:auth.provider,
+                          uid:auth.uid,
+                          email:auth.info.email,
+                          password:Devise.friendly_token[0,20],
+                        )
       end
        
     end
